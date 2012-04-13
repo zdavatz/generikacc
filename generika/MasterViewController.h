921adda3b940ea23ada48ed59863a68d44d5f671
Yasuhiro Asaka <yasaka@ywesee.com>
2012-04-13 19:24:32 +0200

    Added scaned Image in TableView

--- generika/MasterViewController.h
+++ generika/MasterViewController.h
@@ -12,13 +12,7 @@
 
 @interface MasterViewController : UITableViewController < ZBarReaderDelegate >
 {
-    UIImageView *resultImage;
-    UITextView  *resultText;
-
     WebViewController *browser;
-    NSMutableString *_productName;
-    NSMutableString *_productSize;
-
     UITableView *_tableView;
     NSMutableArray *_objects;
     UILabel *_nameLabel;
@@ -28,8 +22,6 @@
     UILabel *_categoryLabel;
     UILabel *_eanLabel;
 }
-@property (nonatomic, retain) IBOutlet UIImageView *resultImage;
-@property (nonatomic, retain) IBOutlet UITextView *resultText;
 @property (strong, nonatomic) DetailViewController *detailViewController;
 
 - (IBAction)scanButtonTapped;
